dc4e5a2520711687a2c1fceef3b5452030fa365a
nmgtz <168343954+nmgtz@users.noreply.github.com>
2024-06-12 07:43:13 +0300

    entertain1.js

---
+++ entertain1.js
@@ -0,0 +1,53 @@
+function entertainWords(matchedentertainWord) {
+    let botResponse = "";
+    if (matchedentertainWord === 'music') {
+                                botResponse = "According to what I last trained on in 2024, Music is an art of sound in time that expresses ideas and emotions in significant forms through the elements of rhythm, melody, harmony, and color. the tones or sounds employed, occurring in single line (melody) or multiple lines (harmony), and sounded or to be sounded by one or more voices or instruments, or both.";
+                            } else if (matchedentertainWord === 'nana' || matchedentertainWord === 'falling' || matchedentertainWord === 'kinyerezi' || matchedentertainWord === 'mpenzi' || matchedentertainWord === 'mtazamaji' || matchedentertainWord === 'kope') {
+                                if (matchedentertainWord === 'nana') {
+                                    var correctWord = "Mr. Nana";
+                                    botResponse = correctWord + " is an east africa music artist. coming from Tanzania and one of the artist that made some smash hit songs like : kinyerezi and now has his own recently song called Falling";
+                                } else if (matchedentertainWord === 'falling') {
+                                    var correctWord = "Mr. Nana";
+                                    botResponse = "It looks like you are looking for the " + ' "' + "Falling" + '" ' + " from " + correctWord + " is one among his song! but also he is an east africa music artist. coming from Tanzania and one of the artist that made some smash hit songs";
+                                } else if (matchedentertainWord === 'Kinyerezi') {
+                                    var correctWord = "Mr. Nana";
+                                    botResponse = "It looks like you are looking for the " + ' "' + "kinyerezi" + '" ' + " from " + correctWord + " is one among his song! but also he is an east africa music artist. coming from Tanzania and one of the artist that made some smash hit songs";
+                                } else if (matchedentertainWord === 'mpenzi') {
+                                    var correctWord = "Mr. Nana";
+                                    botResponse = "It looks like you are looking for the " + ' "' + "Mpenzi Mtazamaji" + '" ' + " from " + correctWord + " is one among his song! but also he is an east africa music artist. coming from Tanzania and one of the artist that made some smash hit songs";
+                                } else if (matchedentertainWord === 'mtazamaji') {
+                                    var correctWord = "Mr. Nana";
+                                    botResponse = "It looks like you are looking for the " + ' "' + "Mpenzi Mtazamaji" + '" ' + " from " + correctWord + " is one among his song! but also he is an east africa music artist. coming from Tanzania and one of the artist that made some smash hit songs";
+                                } else if (matchedentertainWord === 'kope') {
+                                    var correctWord = "Mr. Nana";
+                                    botResponse = "It looks like you are looking for the " + ' "' + "Kope Juu ya Kope" + '" ' + " from " + correctWord + " is one among his song! but also he is an east africa music artist. coming from Tanzania and one of the artist that made some smash hit songs";
+                                }
+                            } else if (matchedentertainWord === 'shirazi' || matchedentertainWord === 'kaka' || matchedentertainWord === 'shiraz') {
+                                if (matchedentertainWord === 'shirazi') {
+                                    var correctWord = "Kaka Shirazi";
+                                    botResponse = '"' + correctWord + '"' + " is a rising comedian from East Africa, and gearing up to bring laughter with his new comedic style. Hailing from Newala Mtwara, Tanzania, in the southern region, he stands out as one of the area's few comedians. " + '"' + correctWord + '"' + " is his stage name,but full name known  as Shirazi Ibrahim Bakari, this emerging comedian from East Africa is ready to bring joy with his fresh comedic style.Currently residing in Dar es Salaam, Shirazi is eagerly preparing to debut his latest work on April 15th this year, promising to entertain audiences with his unique humor";
+                                } else if (matchedentertainWord === 'kaka' || matchedentertainWord === 'shiraz') {
+                                    var correctWord = "Kaka Shirazi";
+                                    botResponse = "It looks like you are looking for the " + '"' + correctWord + '?"' + " okey if is that, " + '"' + correctWord + '"' + " is his satge name, but his real full name is Shirazi Ibrahim Bakari. he is an up-and-coming comedian from East Africa, is set to bring laughter with his fresh comedic style. Originally from Newala Mtwara, Tanzania, in the southern region, he stands out as one of the few comedians from that area. Now living in Dar es Salaam, Shirazi is excited to share his latest work on April 15th this year, promising lots of laughs.";
+                                }
+                            } else if (matchedentertainWord === 'alikiba' || matchedentertainWord === 'ali kiba' || matchedentertainWord === 'ali' || matchedentertainWord === 'kiba') {
+                                if (matchedentertainWord === 'alikiba' || matchedentertainWord === 'ali kiba') {
+                                    botResponse = "Ali Saleh Kiba, better known by his stage name Ali Kiba or Alikiba, is a Tanzanian singer, songwriter, and actor. He is one of the most prominent and successful musicians in East Africa. Alikiba's music career began in the early 2000s, and he quickly rose to fame with his distinctive voice, catchy melodies, and energetic performances. Alikiba is known for his versatility in music genres, including Bongo Flava (a genre that blends Tanzanian hip hop with traditional Tanzanian music), R&B, Afrobeat, and dancehall. His songs often feature themes of love, romance, and everyday life experiences.Some of Alikiba's most popular songs include 'Mwana' 'Chekecha Cheketua,' 'Seduce Me,' and 'Aje.' He has released several successful albums and singles throughout his career, earning him numerous awards and accolades in the East African music industry.In addition to his music career, Alikiba has also ventured into acting, appearing in Tanzanian films and television series.Alikiba's influence extends beyond Tanzania, with his music gaining popularity across Africa and beyond. He remains one of the most respected and celebrated artists in East Africa, known for his talent, charisma, and contributions to the music industry.";
+                                } else if (matchedentertainWord === 'ali') {
+                                    botResponse = "It seems like you're referring to the name 'Ali' without specifying further context. 'Ali' is a common name used in various cultures and regions around the world. It can be a given name or a surname. Popularly,It seems like you're referring to Ali Kiba, the Tanzanian singer and musician. Is there anything specific you'd like to know about him or his music? Feel free to ask!"
+                                } else if (matchedentertainWord === 'kiba') {
+                                    botResponse = "'Kiba' is often used as a nickname for Ali Saleh Kiba, the Tanzanian singer and musician commonly known as Ali Kiba or Alikiba. He is a prominent figure in East African music, known for his versatile musical style and catchy songs. If you have any more questions about Ali Kiba or his music, feel free to ask!"
+                                }
+                            } else if (matchedentertainWord === 'thom' || matchedentertainWord === 'sechuma') {
+                                botResponse = "Thom Sechuma is an stage name as gospel artist... His full name is Thomas Allen Sechuma who is now living  in Dodoma, Tanzania, a vibrant city known for its rich culture and bustling atmosphere. From a young age, Thom showed a deep passion for music and a natural talent for singing. Growing up in a household filled with gospel music, he was inspired by the powerful messages and soul-stirring melodies that filled the air." +
+
+                                    "As Thom pursued his love for music, he quickly gained recognition in the local music scene for his soulful voice and heartfelt lyrics. With his stage name, Thom Sechuma, he embarked on a journey to share his faith and inspire others through his music." +
+
+                                    "One of Thom's most recent songs, 'Itakuwa Shwari,' is a poignant reflection of his unwavering faith and trust in God's plan. The song's lyrics speak of finding peace and comfort in times of struggle, knowing that God is always by our side, guiding and protecting us. With its uplifting melody and heartfelt message, 'Itakuwa Shwari' has resonated deeply with listeners, offering hope and encouragement in difficult times." +
+
+                                    "In addition to 'Itakuwa Shwari' Thom has released several other inspiring songs that have touched the hearts of his fans. 'Rafiki Yangu' celebrates the power of friendship and the joy of companionship, while 'Mwanzo Mpya' speaks of new beginnings and the promise of a brighter future. 'Tupendane' is a heartfelt anthem of love and unity, reminding us of the importance of compassion and understanding in our relationships with others." +
+
+                                    "Through his music, Thom Sechuma has become not only a talented artist but also a beacon of hope and inspiration for his community. His songs uplift the spirit, bring people together, and remind us of the power of faith, love, and perseverance in the face of adversity. As he continues to share his gift with the world, Thom's music will undoubtedly touch the hearts and souls of many, leaving a lasting legacy of faith, hope, and love.";
+                            }
+    return botResponse;
+}
